a3672c2bda15c1ff23e3de20d9a737c4af84de87
Deus Intra <deusintra@yahoo.com>
2020-10-14 01:27:38 +0500

    first commit

---
+++ Assets/Snowball.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Snowball : MonoBehaviour
+{
+    public float speed = 15f;
+    public float lifetime = 2f;
+
+    private Rigidbody _rigidbody;
+
+    private void Awake()
+    {
+        Destroy(gameObject, lifetime);
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        _rigidbody.AddForce(Vector3.forward * speed, ForceMode.VelocityChange);
+    }
+}
---
+++ Assets/Target.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public float secondsToChangeMaterial = 0.5f;
+
+    public Material defaultMaterial;
+    public Material hitMaterial;
+
+    private Renderer _renderer;
+    private Coroutine _coroutine;
+
+    private void Start()
+    {
+        _renderer = GetComponent<Renderer>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Snowball"))
+        {
+            _renderer.material = hitMaterial;
+            if (_coroutine != null)
+            {
+                StopCoroutine(_coroutine);
+            }
+            _coroutine = StartCoroutine(ChangeMaterial());
+        }
+
+        Destroy(collision.gameObject);
+    }
+
+    private IEnumerator ChangeMaterial()
+    {
+        yield return new WaitForSecondsRealtime(secondsToChangeMaterial);
+        _renderer.material = defaultMaterial;
+    }
+}
