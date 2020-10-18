d777198211e6a284135b7bbcb4609e0fad9de45c
Deus Intra <deusintra@yahoo.com>
2020-10-19 03:02:38 +0500

    enemy can shoot

--- Assets/Scripts/EnemyShoot.cs
+++ Assets/Scripts/EnemyShoot.cs
@@ -2,28 +2,35 @@
 
 public class EnemyShoot : MonoBehaviour
 {
-    public Transform snowballSpawnPoint;
+    public Transform snowballSpawn;
     public Transform target;
+    public GameObject snowballPrefab;
 
     private float g;
     private float angle;
 
+    const float hack = 0.55f;
+
     private void Start()
     {
         g = Physics.gravity.y;
-        angle = snowballSpawnPoint.rotation.eulerAngles.x;
+        angle = -snowballSpawn.rotation.eulerAngles.x;
     }
 
     public void ShootAtTarget()
     {
         // v = sqrt( dist * g / sin( 2 * angle ) )
         // assuming target and spawn point have equal height
-        float dist = Vector3.Distance(snowballSpawnPoint.position, target.position);
+        float dist = Vector3.Distance(snowballSpawn.position, target.position);
 
-        snowballSpawnPoint.LookAt(target);
+        snowballSpawn.LookAt(target);
+        snowballSpawn.Rotate(-angle, 0, 0);
 
-        
+        GameObject snowball = Instantiate(snowballPrefab, snowballSpawn.position, snowballSpawn.rotation);
+        Rigidbody snowballRB = snowball.GetComponent<Rigidbody>();
+
+        float snowballVelocityMultiplier = Mathf.Sqrt(dist * g / Mathf.Sin(2 * angle)) * hack;
+
+        snowballRB.AddForce(snowball.transform.forward * snowballVelocityMultiplier, ForceMode.Impulse);
     }
-
-
 }
