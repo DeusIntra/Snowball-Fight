9851bf04f5e091ab93317fc2187599aebd2c5c9e
Deus Intra <deusintra@yahoo.com>
2020-11-17 18:59:30 +0500

    cars

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    private Vector3 _startPosition;
+    private Vector3 _endPosition;
+    private float _speed;
+
+    private float _time;
+
+
+    public void SetDestination(Vector3 destination, float speed = 1f)
+    {
+        _startPosition = transform.position;
+        _endPosition = destination;
+        _speed = speed;
+
+        StartCoroutine(DriveCoroutine());
+    }
+
+
+    private IEnumerator DriveCoroutine()
+    {
+        float t = _time * _speed;
+        while (t < 1)
+        {
+            t = _time * _speed;
+
+            float x = Mathf.Lerp(_startPosition.x, _endPosition.x, t);
+            float z = Mathf.Lerp(_startPosition.z, _endPosition.z, t);
+
+            transform.position = new Vector3(x, transform.position.y, z);
+
+            _time += Time.deltaTime;
+
+            yield return null;
+        }
+
+        Destroy(gameObject);
+        yield break;
+    }
+}
---
+++ Assets/Scripts/CarSpawner.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSpawner : MonoBehaviour
+{
+    public float speed = 1f;
+    public float minSpawnDelaySeconds = 2f;
+    public float maxSpawnDelaySeconds = 7f;
+
+    public List<GameObject> carPrefabs;
+
+    public Transform startPosition;
+    public Transform endPosition;
+
+    private float _time;
+
+
+    private void Start()
+    {
+        startPosition.LookAt(endPosition);
+    }
+
+
+    private void Update()
+    {
+        if (_time <= 0)
+        {
+            Spawn();
+            _time = Random.Range(minSpawnDelaySeconds, maxSpawnDelaySeconds);
+        }
+
+        _time -= Time.deltaTime;
+    }
+
+
+    private void Spawn()
+    {
+        int randIndex = Random.Range(0, carPrefabs.Count);
+
+        GameObject carGO = Instantiate(carPrefabs[randIndex], startPosition.position, startPosition.rotation);
+
+        Car car = carGO.GetComponent<Car>();
+
+        if (car != null)
+        {
+            car.SetDestination(endPosition.position, speed);
+        }
+    }
+}
