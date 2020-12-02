89df6f61b9d7cee74ca3f0ae9a84ee3eaaba0fca
Deus Intra <deusintra@yahoo.com>
2020-12-02 22:12:29 +0500

    menu levels

---
+++ Assets/Scripts/ScreenshotHandler.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public class ScreenshotHandler : MonoBehaviour 
+{
+
+    private static ScreenshotHandler instance;
+
+    private Camera _camera;
+    private bool _takeScreenshotOnNextFrame;
+
+    void OnEnable()
+    {
+        RenderPipelineManager.endCameraRendering += RenderPipelineManager_endCameraRendering;
+    }
+    void OnDisable()
+    {
+        RenderPipelineManager.endCameraRendering -= RenderPipelineManager_endCameraRendering;
+    }
+    private void RenderPipelineManager_endCameraRendering(ScriptableRenderContext context, Camera camera)
+    {
+        OnPostRender();
+    }
+
+    private void Awake() 
+    {
+        instance = this;
+        _camera = gameObject.GetComponent<Camera>();
+        
+    }
+
+    private void OnPostRender() 
+    {
+        Debug.Log("here");
+        if (_takeScreenshotOnNextFrame) 
+        {
+            _takeScreenshotOnNextFrame = false;
+            RenderTexture renderTexture = _camera.targetTexture;
+
+            Texture2D renderResult = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.ARGB32, false);
+            Rect rect = new Rect(0, 0, renderTexture.width, renderTexture.height);
+            renderResult.ReadPixels(rect, 0, 0);
+
+            byte[] byteArray = renderResult.EncodeToPNG();
+            System.IO.File.WriteAllBytes(Application.dataPath + "/CameraScreenshot.png", byteArray);
+            Debug.Log("Saved CameraScreenshot.png");
+
+            RenderTexture.ReleaseTemporary(renderTexture);
+            _camera.targetTexture = null;
+
+        }
+    }
+
+    private void TakeScreenshot(int width, int height) 
+    {
+        _camera.targetTexture = RenderTexture.GetTemporary(width, height, 16);
+        _takeScreenshotOnNextFrame = true;
+    }
+
+    public static void TakeScreenshot_Static(int width, int height) 
+    {
+        instance.TakeScreenshot(width, height);
+    }
+}
